e3e0194f12bb866e0b64a8fcafcbd6c7831e5265
Jared <jspang3@hotmail.com>
2024-12-16 17:04:43 -0500

    broke everything

--- tensor.cpp
+++ tensor.cpp
@@ -2,20 +2,31 @@
 #include <memory>
 #include <string>
 
-std::shared_ptr<int> ptr(new int(3));
-
-class Node {
-
-
-};
+class DifferentiableOp;
+class Add;
+class Prod;
+class Constant;
 
 class DifferentiableOp {
 public: 
 
-    virtual std::shared_ptr<DifferentiableOp> backprop(std::shared_ptr<DifferentiableOp> x) const = 0;
-    virtual double eval() const = 0;
-    virtual std::string toString() const = 0;
+    virtual DifferentiableOp backprop(Variable& x) const {
+        return Constant(0); 
+    };
+    virtual double eval() const {
+        return 0;
+    };
+    virtual std::string toString() const {
+        return "GenericDifferentiableOp";
+    };
     virtual ~DifferentiableOp() = default;
+
+    Add operator+(const DifferentiableOp& other){
+        return Add(*this, other);
+    }
+    Prod operator*(const DifferentiableOp& other){
+        return Prod(*this, other);
+    }
 
 };
 
@@ -26,8 +37,8 @@ private:
 public:
     explicit Constant(double val) : value(val) {}
     
-    std::shared_ptr<DifferentiableOp> backprop(std::shared_ptr<DifferentiableOp> x) const override {
-        return std::make_shared<Constant>(0);
+    DifferentiableOp backprop(Variable& x) const override {
+        return Constant(0);
     }
 
     double eval() const override {
@@ -50,8 +61,8 @@ private:
 public:
     explicit Variable(std::string name, double val = 0) : name(name), value(val) {}
     
-    std::shared_ptr<DifferentiableOp> backprop(std::shared_ptr<DifferentiableOp> x) const override {
-        return x.get() == this ? std::make_shared<Constant>(1) : std::make_shared<Constant>(0);
+    DifferentiableOp backprop(Variable& x) const override {
+        return x.name == name ? Constant(1) : Constant(0);
     }
 
     double eval() const override {
@@ -68,24 +79,57 @@ public:
 
 class Add : public DifferentiableOp { 
 private:
-    std::shared_ptr<DifferentiableOp> x; 
-    std::shared_ptr<DifferentiableOp> y;
+    DifferentiableOp x; 
+    DifferentiableOp y;
 
 public:
-    explicit Add(std::shared_ptr<DifferentiableOp> x, std::shared_ptr<DifferentiableOp> y) : x(x), y(y) {}
+    explicit Add(DifferentiableOp x, DifferentiableOp y) : x(x), y(y) {}
     
-    std::shared_ptr<DifferentiableOp> backprop(std::shared_ptr<DifferentiableOp> var) const override {
-        return std::make_shared<Add>(x->backprop(var), y->backprop(var));
+    DifferentiableOp backprop(Variable& var) const override {
+        return Add(x.backprop(var), y.backprop(var));
     }
 
     double eval() const override {
-        return x->eval() + y->eval();
+        return x.eval() + y.eval();
     }
 
     std::string toString() const override {
-        return " ";
+        return x.toString() + " + " + y.toString();
     }
 
     // Destructor
     ~Add() override = default;
 };
+
+class Prod : public DifferentiableOp { 
+private:
+    DifferentiableOp x; 
+    DifferentiableOp y;
+public:
+    explicit Prod(DifferentiableOp x, DifferentiableOp y) : x(x), y(y) {}
+    
+    DifferentiableOp backprop(Variable& var) const override {
+        return Add(
+            Prod(x.backprop(var),y), Prod(x, y.backprop(var))
+        );
+    }
+
+    double eval() const override {
+        return x.eval() * y.eval();
+    }
+
+    std::string toString() const override {
+        return x.toString() + " * " + y.toString();
+    }
+
+    // Destructor
+    ~Prod() override = default;
+};
+
+
+int main() {
+    Variable x = Variable("x",3);
+    Variable y = Variable("y",2);
+    DifferentiableOp z = (x*x) + (Constant(3)*x*y) + Constant(1);
+    std::cout << z.toString() << std::endl;
+}
